053d66060077168d78531ff77b086af59af3a43c
Abed1313 <abedradwan84.5@gmail.com>
2024-05-01 12:15:39 +0000

    travelD

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -3,11 +3,11 @@ function Footer() {
     return (
         <footer>
             <div className="social-media">
-                <a href="#">Facebook</a>
+                <a href="##">Facebook</a>
                 <br />
-                <a href="#">Twitter</a>
+                <a href="##">Twitter</a>
                 <br />
-                <a href="#">Instagram</a>
+                <a href="##">Instagram</a>
             </div>
              <h3>Author@Abdel Rahman Radwan</h3> 
         </footer>
