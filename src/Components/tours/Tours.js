e4fe8a0b03463ba8842a07da2d2f0e933e489343
Abed1313 <abedradwan84.5@gmail.com>
2024-05-01 12:22:57 +0000

    travelD2

--- src/Components/tours/Tours.js
+++ src/Components/tours/Tours.js
@@ -4,9 +4,11 @@ function Tours(props){
     console.log(11111,props)
     return(
         <>
+        <div className='tour'>
         <h2>{props.traveName}</h2>
         <div>
         <img src={props.traveImg} alt="traveImg" />
+        </div>
         </div>
         </>
     )
